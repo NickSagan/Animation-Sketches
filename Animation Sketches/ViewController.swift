5dcd9e21381a1edf0f058521f5d30071e6e6e8cd
Nikolai Saganenko <9952728@gmail.com>
2021-11-10 15:13:07 +0300

    Ready to try. 16 beautiful animations collected in 1 place.

--- Animation Sketches/ViewController.swift
+++ Animation Sketches/ViewController.swift
@@ -23,33 +23,48 @@ class ViewController: UIViewController {
         
         sender.isHidden = true
         
-        UIView.animate(withDuration: 1, delay: 0, options: []) {
-            switch self.animation {
-            case 0:
-                break
-            default:
-                break
+        if animation < 8 {
+            UIView.animate(withDuration: 1, delay: 0, options: []) {
+                switch self.animation {
+                case 0: self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                case 1: self.imageView.transform = .identity
+                case 2: self.imageView.transform = CGAffineTransform(translationX: -100, y: -50)
+                case 3: self.imageView.transform = CGAffineTransform(translationX: 100, y: 50)
+                case 4: self.imageView.transform = .identity
+                case 5: self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+                case 6: self.imageView.alpha = 0.1
+                    self.imageView.backgroundColor = .systemRed
+                case 7: self.imageView.alpha = 1
+                    self.imageView.backgroundColor = .clear
+                default:
+                    break
+                }
+            } completion: { _ in
+                sender.isHidden = false
             }
-        } completion: { <#Bool#> in
-            <#code#>
+        } else {
+            UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: []) {
+                switch self.animation {
+                case 8: self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                case 8: self.imageView.transform = .identity
+                case 10: self.imageView.transform = CGAffineTransform(translationX: -100, y: -50)
+                case 11: self.imageView.transform = CGAffineTransform(translationX: 100, y: 50)
+                case 12: self.imageView.transform = .identity
+                case 13: self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+                case 14: self.imageView.alpha = 0.1
+                    self.imageView.backgroundColor = .systemRed
+                case 15: self.imageView.alpha = 1
+                    self.imageView.backgroundColor = .clear
+                default:
+                    break
+                }
+            } completion: { _ in
+                sender.isHidden = false
+            }
         }
-        
-//        UIView.animate(withDuration: 1, delay: 0, options: [],
-//           animations: {
-//            switch self.currentAnimation {
-//            case 0:
-//                break
-//
-//            default:
-//                break
-//            }
-//        }) { finished in
-//            sender.isHidden = false
-//        }
 
-        
         animation += 1
-        if animation > 7 { animation = 0 }
+        if animation > 15 { animation = 0 }
     }
     
 }
