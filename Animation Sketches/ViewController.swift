387c6c0bc6a7128e616941583a6ac39a874dbf00
Nikolai Saganenko <9952728@gmail.com>
2021-11-10 14:54:31 +0300

    Add UI

--- Animation Sketches/ViewController.swift
+++ Animation Sketches/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var imageView: UIImageView!
+    var animation = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView = UIImageView(image: UIImage(named: "dragon"))
+        imageView.center = CGPoint(x: view.frame.midX, y: view.frame.midY)
+        view.addSubview(imageView)
     }
 
+    @IBAction func tapped(_ sender: UIButton) {
+        
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, options: []) {
+            switch self.animation {
+            case 0:
+                break
+            default:
+                break
+            }
+        } completion: { <#Bool#> in
+            <#code#>
+        }
+        
+//        UIView.animate(withDuration: 1, delay: 0, options: [],
+//           animations: {
+//            switch self.currentAnimation {
+//            case 0:
+//                break
+//
+//            default:
+//                break
+//            }
+//        }) { finished in
+//            sender.isHidden = false
+//        }
 
+        
+        animation += 1
+        if animation > 7 { animation = 0 }
+    }
+    
 }
 
